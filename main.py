9de9fee63b24a2368d28568d532ca5448abc39bc
muhammad <muhammadkeyton@gmail.com>
2021-08-31 08:28:47 +0300

    postgresql added

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 login_manager = LoginManager()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "njcdscnnsnacnjsdkcl56465415"
-# app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+# app.config['SECRET_KEY'] = "njcdscnnsnacnjsdkcl56465415"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager.init_app(app)
@@ -61,8 +61,8 @@ def admin_only(function):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
