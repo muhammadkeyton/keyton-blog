54a1edd8f28bed9661ae73beb4c4348dc7eac8b8
muhammad <muhammadkeyton@gmail.com>
2021-08-31 08:50:25 +0300

    database create all

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, ForeignKey('blog_posts.id'))
     post = relationship("BlogPost", back_populates="comments")
 
-# db.create_all()
+db.create_all()
 
 
 @app.route('/')
