1e97afb898d8246c43925ab2a508ae62b6c8d76f
muhammad <muhammadkeyton@gmail.com>
2021-09-06 15:57:11 +0300

    custom validation checks added to register

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL,InputRequired,ValidationError
 from flask_ckeditor import CKEditorField
 
 ##WTForm
@@ -10,10 +10,17 @@ class CreatePostForm(FlaskForm):
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
+def password_check(form,field):
+    if len(field.data) < 8:
+        raise ValidationError('password must be atleast 8 characters long.')
+
+def email_check(form,field):
+    if "@" not in field.data:
+        raise ValidationError("Invalid email address make sure you type in a real email address must contain'@'.eg('someone@gmail.com')")
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    email = StringField("Email", validators=[InputRequired(),email_check])
+    password = PasswordField("Password", validators=[InputRequired(),password_check])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Register")
 
